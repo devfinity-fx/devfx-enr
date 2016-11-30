d91821cfce3437f502bd266374370b135da2c33f
Era Azana <azanaera691@gmail.com>
2016-11-30 12:34:36 +0800

    Added MDL

--- trunk/resources/views/app.blade.php
+++ trunk/resources/views/app.blade.php
@@ -1,7 +1,11 @@
 <html>
 <head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+<link href="{{ asset('/css/styles.css') }}" rel="stylesheet"> 
 <style>
-    FORM DT {
+    /*FORM DT {
         clear:both;
         width:33%;
         float:left;
@@ -12,10 +16,16 @@
         float:left;
         width:66%;
         margin:0 0 0.5em 0.25em;
-    }
+    }*/
+
+    html, body {
+    font-family: 'Roboto', 'Helvetica', sans-serif;
+    background-color: #95a5a6;
+}    
 </style>
 </head>
 <body>
     @yield('content')
+<script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
 </body>
 </html>
--- trunk/resources/views/enrollment/new.blade.php
+++ trunk/resources/views/enrollment/new.blade.php
@@ -1,45 +1,85 @@
 @extends('app')
 @section('content')
     {!! Form::open(['url' => 'enrollment/submit']) !!}
-    <dl>
-        <dt>{!! Form::label('IDNo', 'ID #') !!} </dt>
-        <dd>{!! Form::text('IDNo') !!} </dd>
-        <dt>{!! Form::label('LastName', 'Last Name') !!} </dt>
-        <dd>{!! Form::text('LastName') !!} </dd>
-        <dt>{!! Form::label('FirstName', 'First Name') !!} </dt>
-        <dd>{!! Form::text('FirstName') !!} </dd>
-        <dt>{!! Form::label('MiddleName', 'Middle Name') !!} </dt>
-        <dd>{!! Form::text('MiddleName') !!} </dd>
-        <dt>{!! Form::label('Gender', 'Gender') !!} </dt>
-        <dd>{!! Form::select('Gender', ['-'=>'- Select -','M'=>'Male','F'=>'Female']) !!} </dd>
-        <dt>{!! Form::label('DateOfBirth', 'Date of Birth') !!} </dt>
-        <dd>{!! Form::text('DateOfBirth') !!} </dd>
-        <dt>{!! Form::label('StreetAddress', 'Street Address') !!} </dt>
-        <dd>{!! Form::text('StreetAddress') !!} </dd>
-        <dt>{!! Form::label('Village', 'Village') !!} </dt>
-        <dd>{!! Form::text('Village') !!} </dd>
-        <dt>{!! Form::label('City', 'City') !!} </dt>
-        <dd>{!! Form::text('City') !!} </dd>
-        <dt>{!! Form::label('Province', 'Province') !!} </dt>
-        <dd>{!! Form::text('Province', '', ['id'=>'shit']) !!} </dd>
-        <dt>{!! Form::label('Nationality', 'Nationality') !!} </dt>
-        <dd>{!! Form::text('Nationality') !!} </dd>
-        <dt>{!! Form::label('CivilStatus', 'Civil Status') !!} </dt>
-        <dd>{!! Form::select('CivilStatus', ['-'=>'- Select -','S'=>'Single','M'=>'Married']) !!} </dd>
-        <dt>{!! Form::label('Religion', 'Religion') !!} </dt>
-        <dd>{!! Form::text('Religion') !!} </dd>
-        <dt>{!! Form::label('EmailAddress', 'Email Address') !!} </dt>
-        <dd>{!! Form::text('EmailAddress') !!} </dd>
-        <dt>{!! Form::label('ContactNo', 'Contact #') !!} </dt>
-        <dd>{!! Form::text('ContactNo') !!} </dd>
-        <dt>{!! Form::label('Username', 'Username') !!} </dt>
-        <dd>{!! Form::text('Username') !!} </dd>
-        <dt>{!! Form::label('Password', 'Password') !!} </dt>
-        <dd>{!! Form::password('Password') !!} </dd>
-        <dt>{!! Form::label('AccountLevel') !!} </dt>
-        <dd>{!! Form::select('AccountLevel', ['-'=>'- Select -','S'=>'Student','T'=>'Teacher','A'=>'Accountant','R'=>'Registrar','Z'=>'Administrator','D'=>'Developer']) !!} </dd>
-    </dl>
-    {!! Form::submit('submit') !!}
-    {!! Form::reset('reset') !!}
-    {!! Form::close() !!}
+
+
+            <form action="#"> 
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 {!! Form::text('IDNo', '', ['class' => 'mdl-textfield__input', 'id' => 'IDno']) !!}
+                 {!! Form::label('IDNo', 'ID #', ['class' => 'mdl-textfield__label']) !!}
+                 </div>
+             </form>
+
+             <form action="#"> 
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 {!! Form::text('LastName', '', ['class' => 'mdl-textfield__input', 'id' => 'IDno']) !!}
+                 {!! Form::label('LastName', 'LastName', ['class' => 'mdl-textfield__label']) !!}
+                 </div>
+             </form>
+
+             <form action="#"> 
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 {!! Form::text('FirstName', '', ['class' => 'mdl-textfield__input', 'id' => 'IDno']) !!}
+                 {!! Form::label('FirstName', 'FirstName', ['class' => 'mdl-textfield__label']) !!}
+                 </div>
+             </form>             
+
+             <form action="#"> 
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 {!! Form::text('MiddleName', '', ['class' => 'mdl-textfield__input', 'id' => 'IDno']) !!}
+                 {!! Form::label('MiddleName', 'MiddleName', ['class' => 'mdl-textfield__label']) !!}
+                 </div>
+             </form>             
+
+             <form action="#"> 
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 {!! Form::text('Gender', '', ['class' => 'mdl-textfield__input', 'id' => 'IDno']) !!}
+                 {!! Form::label('Gender', 'Gender', ['class' => 'mdl-textfield__label']) !!}
+                 </div>
+             </form>
+
+             <button id="demo-menu-lower-left" class="mdl-button mdl-js-button mdl-button--icon">
+                <i class="material-icons">more_vert</i>
+             </button>
+
+            <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+                for="demo-menu-lower-left">
+                  <li class="mdl-menu__item">Some Action</li>
+                  <li class="mdl-menu__item mdl-menu__item--full-bleed-divider">Another Action</li>
+                  <li disabled class="mdl-menu__item">Disabled Action</li>
+                  <li class="mdl-menu__item">Yet Another Action</li>
+            </ul>
+
+             <br><br>
+             {!! Form::label('Gender', 'Gender') !!}  
+             {!! Form::select('Gender', ['-'=>'- Select -','M'=>'Male','F'=>'Female']) !!}  
+             {!! Form::label('DateOfBirth', 'Date of Birth') !!}  
+             {!! Form::text('DateOfBirth') !!}  
+             {!! Form::label('StreetAddress', 'Street Address') !!}  
+             {!! Form::text('StreetAddress') !!}  
+             {!! Form::label('Village', 'Village') !!}  
+             {!! Form::text('Village') !!}  
+             {!! Form::label('City', 'City') !!}  
+             {!! Form::text('City') !!}  
+             {!! Form::label('Province', 'Province') !!}  
+             {!! Form::text('Province', '', ['id'=>'shit']) !!}  
+             {!! Form::label('Nationality', 'Nationality') !!}  
+             {!! Form::text('Nationality') !!}  
+             {!! Form::label('CivilStatus', 'Civil Status') !!}  
+             {!! Form::select('CivilStatus', ['-'=>'- Select -','S'=>'Single','M'=>'Married']) !!}  
+             {!! Form::label('Religion', 'Religion') !!}  
+             {!! Form::text('Religion') !!}  
+             {!! Form::label('EmailAddress', 'Email Address') !!}  
+             {!! Form::text('EmailAddress') !!}  
+             {!! Form::label('ContactNo', 'Contact #') !!}  
+             {!! Form::text('ContactNo') !!}  
+             {!! Form::label('Username', 'Username') !!}  
+             {!! Form::text('Username') !!}  
+             {!! Form::label('Password', 'Password') !!}  
+             {!! Form::password('Password') !!}  
+             {!! Form::label('AccountLevel') !!}  
+             {!! Form::select('AccountLevel', ['-'=>'- Select -','S'=>'Student','T'=>'Teacher','A'=>'Accountant','R'=>'Registrar','Z'=>'Administrator','D'=>'Developer']) !!} 
+            {!! Form::submit('submit') !!}
+            {!! Form::reset('reset') !!}
+            {!! Form::close() !!}
 @endsection()
